8824c49499f634a5a12eafd7f4b04d4ac668b853
Jagi Natarajan <jagnat4000@gmail.com>
2026-09-17 13:21:08 -0400

    Radial rings

---
+++ phyllotaxis-sketch-template/setenvs.sh
@@ -0,0 +1,4 @@
+export PHYLLO_HOST=phyllotaxis.local
+export PHYLLO_SLOT=1
+export PHYLLO_NAME=supervoronoi
+export PHYLLO_COLOR='#00ff00'
--- phyllotaxis-sketch-template/src/sketch.rs
+++ phyllotaxis-sketch-template/src/sketch.rs
@@ -6,22 +6,36 @@ use crate::phyllo::{self, Rgb, Frame, Input, LED_COUNT};
 pub const CAPABILITIES: u32 = phyllo::capabilities::INPUT;
 
 #[unsafe(no_mangle)]
-static mut idx: usize = 0;
+static mut idx: i32 = 0;
 
 pub fn render(input: &Input<'_>, frame: &mut Frame<'_>) {
     unsafe{
     let time = input.phase(8_000_000);
 
+    idx += input.encoder_delta();
+    if idx < 0 {
+        idx = 89;
+    }
+    idx %= 90;
+
     for led in 0..LED_COUNT {
-        if led == idx {
+        let mod18 = led % 18;
+        if led == idx as usize {
             frame.set(led, Rgb::new(255, 0, 0));
-        }
-        else {
+        } else if [4, 5, 14, 15].contains(&mod18) {
+            frame.set(led, Rgb::new(0, 255, 0));
+        } else if [3, 6, 13, 16].contains(&mod18) {
+            frame.set(led, Rgb::new(0, 0, 255));
+        } else if [2, 7, 12, 17].contains(&mod18) {
+            frame.set(led, Rgb::new(200, 0, 200));
+        } else if [0, 1, 8, 11].contains(&mod18) {
+            frame.set(led, Rgb::new(200, 200, 0));
+        } else if [9, 10].contains(&mod18) {
+            frame.set(led, Rgb::new(0, 200, 200));
+        } else {
             frame.set(led, Rgb::new(0, 0, 0));
         }
     }
-    idx += 1;
-    idx = idx % 90;
     }
 }
 
